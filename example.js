adf14178805070c60db27e99708fe68f40d589f7
Skylar Stein <skylar@peringer.com>
2016-10-26 21:20:57 -0700

    update example code

--- example.js
+++ example.js
@@ -12,9 +12,14 @@ const hih6130 = new HIH6130(options);
 
 const readSensorData = () => {
   hih6130.readSensorData()
-    .then(data => console.log(`data = ${JSON.stringify(data, null, 2)}`))
-    .catch(err => console.log(`HIH6130 read error: ${err}`))
-  setTimeout(readSensorData, 2000);
+    .then((data) => {
+      console.log(`data = ${JSON.stringify(data, null, 2)}`);
+      setTimeout(readSensorData, 2000);
+    })
+    .catch((err) => {
+      console.log(`HIH6130 read error: ${err}`);
+      setTimeout(readSensorData, 2000);
+    });
 }
 
 readSensorData();
